1006f42588ba6a3b3582a724a9c75acbbece5507
hXR16F <hxr16f.ar@gmail.com>
2020-07-04 02:21:34 +0200

    Add files via upload

---
+++ settings.bat
@@ -0,0 +1,11 @@
+:: Force using colors
+set force_colors=false
+
+:: Use UTF-8 encoding
+set utf-8=true
+
+:: Start WINDOWS TERMINAL with theme
+set windows_terminal=true
+
+:: Split WINDOWS TERMINAL into two panes with CMD and WSL
+set windows_terminal.split=true
