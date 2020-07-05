351e301ce3c6c725eb42bc51bd626782c193305a
Nimplex <39964594+Nimplex@users.noreply.github.com>
2020-07-05 16:10:11 +0200

    better version

---
+++ settings.bat
@@ -0,0 +1,14 @@
+:: Where hyperial is. 
+set dir=C:\hyperial
+
+:: Force using colors.
+set force_colors=true
+
+:: Use UTF-8 encoding.
+set utf-8=true
+
+:: Start theme with windows terminal, if false hyperial will start in new cmd window.
+set windows_terminal=true
+
+:: Split WINDOWS TERMINAL into two panes with CMD and WSL.
+set windows_terminal.split=false
